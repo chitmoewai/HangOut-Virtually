971f840ada569a83a33d056a9ac1c180fc8e3476
Chit Moe Wai <chitmoewai135@gmail.com>
2022-12-05 15:00:12 +0630

    chitmoe : Login Scene and Connecting to Photon Server

---
+++ SocialVR/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LoginManager : MonoBehaviourPunCallbacks
+{
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This method will connect us to Photon.
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Photon Callback Methods
+
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnnected is called. The server is available!");
+    }
+
+    // this method is called when user is successfully connected to the Photon server.
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server!");
+    }
+
+    #endregion
+}
